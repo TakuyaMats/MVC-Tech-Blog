ee1f81c10c715cc491676cf50c67f11a5a524844
TMats <takomats@hotmail.com>
2021-05-10 19:14:00 -0400

    added comment and blog models

--- models/index.js
+++ models/index.js
@@ -20,7 +20,16 @@ Blog.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = {
     User,
-    Project
+    Comment, Blog
 };
